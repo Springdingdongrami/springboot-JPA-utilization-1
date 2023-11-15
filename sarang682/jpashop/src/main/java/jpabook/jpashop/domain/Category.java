1d20afc1eed567bc1046ec01618304b2d8464f86
박사랑 <jms20914@gmail.com>
2023-11-15 14:14:57 +0900

    2. 엔티티 설계시 주의점

--- sarang682/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ sarang682/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,26 +8,36 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class Category {
 
-    @Id @GeneratedValue
-    @Column(name="category_id")
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
     private Long id;
 
     private String name;
 
     @ManyToMany
-    @JoinTable(name="category_item",
-            joinColumns = @JoinColumn(name="category_id"),
-            inverseJoinColumns = @JoinColumn(name="item_id"))
-    private List<Item> items=new ArrayList<>();
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //==연관 관계 메서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
--- sarang682/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ sarang682/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,20 +6,23 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class OrderItem {
 
     @Id
     @GeneratedValue
-    @Column(name="order_item_id")
+    @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name="item_id")
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
